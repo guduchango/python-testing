9690f7df3cbb9a7801ce0c52c602266c015d6f70
Edgardo Ponce <ed.poncepaez@gmail.com>
2025-01-08 20:12:12 -0300

    step_2 setup usage function

--- tests/tests_calculator.py
+++ tests/tests_calculator.py
@@ -5,7 +5,7 @@ from src.calculator import sum, subtract
 class CalculatorTests(unittest.TestCase):
 
     def test_sum(self):
-        assert sum(2, 3) == 6
+        assert sum(2, 4) == 6
 
     def test_subtract(self):
         assert subtract(10, 5) == 5
